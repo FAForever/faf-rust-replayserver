5f37d209c278fdff58a9e108f7edbf242307b950
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2020-11-26 00:57:29 +0100

    Better error type
    A very educating experience in error handling. Probably will need
    tweaks.
    
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- src/accept/header.rs
+++ src/accept/header.rs
@@ -1,6 +1,8 @@
+use std::io::ErrorKind;
 use std::str::from_utf8;
 
-use crate::error::{ConnResult, ToCerr};
+use crate::as_conn_err;
+use crate::error::{ConnResult,AddContext};
 use crate::error::ConnectionError;
 use crate::server::connection::Connection;
 
@@ -22,7 +24,7 @@ async fn read_type(conn: &mut Connection) -> ConnResult<ConnectionType>
     match &buf {
         b"P/" => Ok(ConnectionType::WRITER),
         b"G/" => Ok(ConnectionType::READER),
-        _ => Err(ConnectionError {})
+        _ => Err(format!("Invalid connection type: {:x?}", buf).into())
     }
 }
 
@@ -32,18 +34,24 @@ async fn read_game_data(conn: &mut Connection) -> ConnResult<(u64, String)>
     conn.read_until(b'\0', &mut line, 1024).await?;
     let pieces: &Vec<&[u8]> = &line[..].split(|val| {val == &b'/'}).collect();
     if pieces.len() != 2 {
-        return Err(ConnectionError {})
+        return Err(ConnectionError::BadData(format!("Connection header has wrong number of slashes: {}", pieces.len())));
     }
 
-    let id: u64 = from_utf8(pieces[0]).tocerr()?
-                  .parse::<u64>().map_err(|_| {ConnectionError {}})?;
-    let name: String = String::from(from_utf8(pieces[1]).tocerr()?);
+    let id = as_conn_err!(u64, from_utf8(pieces[0])?.parse::<u64>()?,
+                          "Failed to parse replay ID");
+    let name = as_conn_err!(String, String::from(from_utf8(pieces[1])?),
+                            "Failed to decode connection string id");
     Ok((id, name))
 }
 
 pub async fn read_connection_info(conn: &mut Connection) -> ConnResult<ConnectionInfo> // FIXME error type
 {
-    let type_ = read_type(conn).await?;
+    let type_ = read_type(conn).await.map_err(|e| {
+        match e {
+            ConnectionError::IO{source: e} if e.kind() == ErrorKind::UnexpectedEof => ConnectionError::NoData(),
+            e => e,
+        }
+    })?;
     let (id, name) = read_game_data(conn).await?;
     Ok(ConnectionInfo {type_, id, name})
 }
--- src/error.rs
+++ src/error.rs
@@ -1,44 +1,76 @@
-use std::error::Error;
-use std::fmt::Display;
+use std::num::ParseIntError;
 use std::str::Utf8Error;
 
-#[derive(Debug)]
-pub struct ConnectionError {
-}
+use thiserror::Error;
 
-impl Display for ConnectionError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "TODO")
-    }
-}
-
-impl Error for ConnectionError {
-}
-
-impl From<std::io::Error> for ConnectionError {
-    fn from(err: std::io::Error) -> ConnectionError {
-        ConnectionError {}  // FIXME
-    }
-}
-
-impl From<Utf8Error> for ConnectionError {
-    fn from(err: Utf8Error) -> ConnectionError {
-        ConnectionError {}  // FIXME
+#[derive(Error, Debug)]
+pub enum ConnectionError {
+    #[error("Empty connection")]
+    NoData(),
+    #[error("Bad data: {0}")]
+    BadData(String),
+    #[error("IO error: {source}")]
+    IO {
+        #[from]
+        source: std::io::Error,
     }
 }
 
 pub type ConnResult<T> = Result<T, ConnectionError>;
 
-pub trait ToCerr {
-    type Return;
-    type Err: Into<ConnectionError>;
-    fn tocerr(self) -> Result<Self::Return, ConnectionError>;
+// Below is some magic for nicer handling of error chains.
+
+impl From<String> for ConnectionError {
+    fn from(s : String) -> Self {
+        Self::BadData(s)    // That's the most common error type
+    }
 }
 
-impl<R, E: Into<ConnectionError>> ToCerr for Result<R, E> {
-    type Return = R;
-    type Err = E;
-    fn tocerr(self) -> Result<R, ConnectionError> {
-        self.map_err(E::into)
+impl From<&str> for ConnectionError {
+    fn from(s : &str) -> Self {
+        Self::BadData(String::from(s))
+    }
+}
+
+// Add string context to errors.
+pub trait AddContext<T> {
+    fn context(self, c: T) -> Self;
+}
+
+impl<T> AddContext<String> for ConnResult<T> {
+    fn context(self, s: String) -> Self {
+        match self {
+            Err(ConnectionError::BadData(_)) => Err(ConnectionError::BadData(s)),
+            e => e,
+        }
+    }
+}
+
+impl<T> AddContext<&str> for ConnResult<T> {
+    fn context(self, s: &str) -> Self {
+        self.context(String::from(s))
+    }
+}
+
+// A hacky substitute for try-blocks. Don't use in hot code.
+#[macro_export]
+macro_rules! as_conn_err {
+    ( $t: ty, $e: expr, $s: expr) => {
+        || -> ConnResult<$t> {
+            Ok($e)
+        }().context($s)?
+    }
+}
+
+// Typical conversions.
+impl From<Utf8Error> for ConnectionError {
+    fn from(_ :Utf8Error) -> Self {
+        "UTF decode error".into()
+    }
+}
+
+impl From<ParseIntError> for ConnectionError {
+    fn from(_ :ParseIntError) -> Self {
+        "Int parse error".into()
     }
 }
