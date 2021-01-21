b1ef3a371a20c4956e4e2df75036837dd098c16a
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2021-01-22 00:06:32 +0100

    +1 test
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- src/replay/receive/quorum_merge_strategy.rs
+++ src/replay/receive/quorum_merge_strategy.rs
@@ -866,224 +866,43 @@ mod tests {
             assert_eq!(out_buf[3..8], [4, 5, 6, 7, 8]);
         }
     }
+
+    #[test]
+    fn test_strategy_later_has_more_data() {
+        let mut strat = QuorumMergeStrategy::new();
+        let stream1 = Rc::new(RefCell::new(WriterReplay::new()));
+        let stream2 = Rc::new(RefCell::new(WriterReplay::new()));
+
+        stream1.borrow_mut().add_header(ReplayHeader { data:vec!(1, 3, 3, 7) });
+        stream2.borrow_mut().add_header(ReplayHeader { data:vec!(1, 3, 3, 7) });
+        let token1 = strat.replay_added(stream1.clone());
+        let token2 = strat.replay_added(stream2.clone());
+        strat.replay_header_added(token1);
+        strat.replay_header_added(token2);
+
+        stream1.borrow_mut().add_data(&[1, 2, 3, 4]);
+        stream1.borrow_mut().set_delayed_data_progress(4);
+        strat.replay_data_updated(token1);
+
+        stream2.borrow_mut().add_data(&[1, 2, 3, 4, 5, 6]);
+        stream2.borrow_mut().set_delayed_data_progress(6);
+        strat.replay_data_updated(token2);
+
+        stream1.borrow_mut().finish();
+        stream2.borrow_mut().finish();
+        strat.replay_removed(token1);
+        strat.replay_removed(token2);
+        strat.finish();
+
+        let out_stream_ref = strat.get_merged_replay();
+        let out_stream = out_stream_ref.borrow();
+        let out_data = out_stream.get_data();
+
+        assert!(out_data.len() == 6);
+        let out_buf: &mut [u8] = &mut [0; 6];
+        out_data.reader().read(out_buf).unwrap();
+        assert_eq!(out_buf, &[1, 2, 3, 4, 5, 6]);
+    }
 }
 
-// TODO convert these python server tests to rust.
-/*
-@pytest.mark.parametrize("trimming", [None, 3])
-def test_strategy_later_has_more_data(trimming,
-                                      outside_source_stream):
-    conf = MockStrategyConfig()
-    conf.stream_comparison_cutoff = trimming
-    strat = QuorumMergeStrategy.build(outside_source_stream, conf)
-    stream1 = MockStream()
-    stream2 = MockStream()
-    stream2._header = "Header"
-
-    strat.stream_added(stream1)
-    strat.stream_added(stream2)
-    strat.new_header(stream2)
-
-    stream1._add_data(b"Data")
-    strat.new_data(stream1)
-    stream2._add_data(b"Data and stuff")
-    strat.new_data(stream2)
-
-    strat.stream_removed(stream2)
-    strat.stream_removed(stream1)
-    strat.finalize()
-    assert outside_source_stream.data.bytes() == b"Data and stuff"
-
-
-# TODO - extend this one
-@pytest.mark.parametrize("trimming", [None, 3])
-def test_strategy_tracked_stream_diverges(
-        trimming, outside_source_stream):
-    conf = MockStrategyConfig()
-    conf.stream_comparison_cutoff = trimming
-    strat = QuorumMergeStrategy.build(outside_source_stream, conf)
-    stream1 = MockStream()
-    stream2 = MockStream()
-    stream2._header = "Header"
-
-    strat.stream_added(stream1)
-    strat.stream_added(stream2)
-    strat.new_header(stream2)
-
-    stream1._add_data(b"Data and stuff")
-    strat.new_data(stream1)
-    strat.stream_removed(stream1)
-    stream2._add_data(b"Data and smeg and blahblah")
-    strat.new_data(stream2)
-    strat.stream_removed(stream2)
-    strat.finalize()
-    assert outside_source_stream.data.bytes() in [
-        b"Data and stuff",
-        b"Data and smeg and blahblah"
-    ]
-
-
-# TODO - add tests with stalling connections
-
-@pytest.mark.parametrize("trimming", [None, 3])
-def test_strategy_quorum_newly_arrived_quorum(
-        trimming, outside_source_stream):
-    conf = MockStrategyConfig()
-    conf.stream_comparison_cutoff = trimming
-    strat = QuorumMergeStrategy.build(outside_source_stream, conf)
-    stream1 = MockStream()
-    stream2 = MockStream()
-    stream3 = MockStream()
-    stream1._header = "Header"
-    stream2._header = "Header"
-    stream3._header = "Header"
-
-    strat.stream_added(stream1)
-    strat.stream_added(stream2)
-    strat.stream_added(stream3)
-
-    strat.new_header(stream1)
-    stream1._add_data(b"Data and stuff")
-    strat.new_data(stream1)
-    strat.stream_removed(stream1)
-
-    stream2._add_data(b"Data and smeg and blahblah")
-    strat.new_data(stream2)
-    stream3._add_data(b"Data and smeg and blahblah")
-    strat.new_data(stream3)
-    strat.stream_removed(stream2)
-    strat.stream_removed(stream3)
-    strat.finalize()
-    assert outside_source_stream.data.bytes() == b"Data and smeg and blahblah"
-
-
-@pytest.mark.parametrize("trimming", [None, 2])
-def test_quorum_strategy_uses_future_data(trimming, outside_source_stream):
-    conf = MockStrategyConfig()
-    conf.stream_comparison_cutoff = trimming
-    strat = QuorumMergeStrategy.build(outside_source_stream, conf)
-    stream1 = MockStream(True)
-    stream2 = MockStream(True)
-    stream3 = MockStream(True)
-    stream1._header = "Header"
-    stream2._header = "Header"
-    stream3._header = "Header"
-
-    strat.stream_added(stream1)
-    strat.stream_added(stream2)
-    strat.stream_added(stream3)
-
-    stream1._future_data += b"foo"
-    stream2._future_data += b"foo"
-    stream1._add_data(b"f")
-    stream2._add_data(b"fo")
-
-    # First check calculating quorum point.
-    # We expect to send f, since 2 future data items confirm it.
-    # Note that we only react to streams when new_data is called, future_data
-    # is strictly for comparison purposes.
-    strat.new_data(stream1)
-    strat.new_data(stream2)
-    assert outside_source_stream.data.bytes() == b"fo"
-    stream1._add_data(b"oo")
-    strat.new_data(stream1)
-    assert outside_source_stream.data.bytes() == b"foo"
-
-    # And now stalemate resolution.
-    # After resolution, we should have confirmed "foo aaaaa", so sending some
-    # "a"s should come through.
-    stream3._future_data += b"foo aaaaa"
-    stream1._future_data += b" bbbbb"
-    stream2._future_data += b" aaaaa"
-    stream1._add_data(b" bbbbb")
-    stream2._add_data(b"o aaa")
-    stream3._add_data(b"foo")
-    strat.new_data(stream1)
-    strat.new_data(stream2)
-    strat.new_data(stream3)
-    assert outside_source_stream.data.bytes() == b"foo aaa"
-
-
-# This one kind of relies on internals? Maybe we should allow querying what
-# role a stream has?
-@pytest.mark.parametrize("trimming", [None, 3])
-def test_quorum_strategy_immediate_stalemate_resolve(trimming,
-                                                     outside_source_stream):
-    conf = MockStrategyConfig()
-    conf.stream_comparison_cutoff = trimming
-    strat = QuorumMergeStrategy.build(outside_source_stream, conf)
-    stream1 = MockStream(True)
-    stream2 = MockStream(True)
-    stream3 = MockStream(True)
-    stream1._header = "Header"
-    stream2._header = "Header"
-    stream3._header = "Header"
-
-    strat.stream_added(stream1)
-    strat.stream_added(stream2)
-    strat.stream_added(stream3)
-
-    stream1._future_data += b"foo"
-    stream2._future_data += b"foo"
-    stream1._add_data(b"f")
-    stream2._add_data(b"f")
-
-    strat.new_data(stream1)
-    strat.new_data(stream2)
-
-    # Streams 1 and 2 should make a quorum now.
-    assert outside_source_stream.data.bytes() == b"f"
-
-    stream1._future_data += b"aaa"
-    stream2._future_data += b"bbb"
-    stream3._future_data += b"fooaaa"
-    stream1._add_data(b"ooaaa")
-    stream2._add_data(b"oobbb")
-    stream3._add_data(b"fooaaa")
-
-    strat.new_data(stream3)     # This one's not tracked
-    assert outside_source_stream.data.bytes() == b"f"
-    strat.new_data(stream2)
-    # Now we should've entered a stalemate, then resolved it with stream 3.
-    assert outside_source_stream.data.bytes() == b"fooaaa"
-
-
-def test_quorum_strategy_accepts_cutoff(outside_source_stream):
-    config = MockStrategyConfig()
-    config.stream_comparison_cutoff = 4
-    strat = QuorumMergeStrategy.build(outside_source_stream,
-                                      MockStrategyConfig())
-
-    stream1 = MockStream(True)
-    stream2 = MockStream(True)
-    stream3 = MockStream(True)
-    stream1._header = "Header"
-    stream2._header = "Header"
-    stream3._header = "Header"
-    strat.stream_added(stream1)
-    strat.stream_added(stream2)
-    strat.stream_added(stream3)
-
-    # No verification, just check if nothing breaks
-
-    stream1._future_data += b"abcdefg"
-    stream1._add_data(b"abcdefg")
-    strat.new_data(stream1)
-    stream2._future_data += b"abcdefg"
-    stream2._add_data(b"abcdefg")
-    strat.new_data(stream2)
-    assert outside_source_stream.data.bytes() == b"abcdefg"
-
-    stream1._future_data += b"h"
-    stream1._add_data(b"h")
-    stream2._future_data += b"i"
-    stream2._add_data(b"i")
-    strat.new_data(stream1)
-    strat.new_data(stream2)
-
-    # Check cutoff side effects, just to be sure it's enabled
-    stream3._future_data += b"aaadefgh"
-    stream3._add_data(b"aaadefgh")
-    strat.new_data(stream3)
-    assert outside_source_stream.data.bytes() == b"abcdefgh"
-*/
+// TODO add more tests. Possibly fuzzing. Not all python server tests made sense to convert.
