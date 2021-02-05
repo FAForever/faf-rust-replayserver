e38937b4d40c29d8738aadf721127fc21064ccba
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2021-02-05 14:33:51 +0100

    Exit server process (not just thread) on any panic
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -36,8 +36,17 @@ async fn run_server() {
     /* Worker threads are joined once we drop the server. */
 }
 
+fn setup_process_exit_panic_hook() {
+    let orig_hook = std::panic::take_hook();
+    std::panic::set_hook(Box::new(move |panic_info| {
+        orig_hook(panic_info);
+        std::process::exit(1);
+    }));
+}
+
 pub fn run() -> () {
     env_logger::init();
+    setup_process_exit_panic_hook();
     let local_loop = tokio::runtime::Builder::new_current_thread()
         .build()
         .unwrap();
