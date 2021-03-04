247eb7b54092b5b4eb4c1d10d2971588acda99da
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2021-03-05 00:19:35 +0100

    Pass cargo test args in coverage script
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- test/run_coverage.sh
+++ test/run_coverage.sh
@@ -10,13 +10,13 @@ rm -rf ./coverage_run/debug/deps/*.gcno
 export RUSTFLAGS="-Zinstrument-coverage"
 
 env CARGO_TARGET_DIR=./coverage_build cargo build
-env CARGO_TARGET_DIR=./coverage_build LLVM_PROFILE_FILE=default.profraw cargo test
+env CARGO_TARGET_DIR=./coverage_build LLVM_PROFILE_FILE=default.profraw cargo test $@
 
 export CARGO_INCREMENTAL=0
 export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
 export RUSTDOCFLAGS="-Cpanic=abort"
 
 env CARGO_TARGET_DIR=./coverage_run cargo build
-env CARGO_TARGET_DIR=./coverage_run cargo test
+env CARGO_TARGET_DIR=./coverage_run cargo test $@
 
 grcov . --binary-path ./coverage_run/debug/ -s . -t html --branch --ignore-not-existing --ignore "/*" -o ./coverage
