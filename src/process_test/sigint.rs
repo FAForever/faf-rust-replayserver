3487f0d05578793ed0a12654903c561a843eee6b
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2021-05-09 21:24:20 +0200

    Add process tests to CI
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- src/process_test/sigint.rs
+++ src/process_test/sigint.rs
@@ -2,6 +2,7 @@ use faf_rust_replayserver::util::process::wait_for_signals;
 
 #[tokio::main]
 async fn main() {
+    println!("Waiting for sigint");
     wait_for_signals().await;
     println!("Received a sigint");
 }
