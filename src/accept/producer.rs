45f2cdff9437f2380a70e5e7b6f235c3cf4fe242
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2025-04-19 12:21:29 +0200

    Websocket listen function
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- src/accept/producer.rs
+++ src/accept/producer.rs
@@ -1,4 +1,4 @@
-use crate::server::connection::Connection;
+use crate::server::{connection::Connection, websocket_stream::make_split_websocket_from_tcp};
 use futures::{Stream, StreamExt};
 use tokio::net::TcpListener;
 use tokio_stream::wrappers::TcpListenerStream;
@@ -12,6 +12,27 @@ pub async fn tcp_listen(addr: String) -> impl Stream<Item = Connection> {
                 None
             }
             Ok(s) => Some(Connection::new(s)),
+        }
+    })
+}
+
+pub async fn websocket_listen(addr: String) -> impl Stream<Item = Connection> {
+    let listener = TcpListener::bind(addr).await.unwrap();
+    TcpListenerStream::new(listener).filter_map(|c| async {
+        match c {
+            Err(e) => {
+                log::info!("Failed to accept connection: {}", e);
+                None
+            }
+            Ok(s) => {
+                match make_split_websocket_from_tcp(s).await {
+                    Err(e) => {
+                        log::info!("Failed to create websocket: {}", e);
+                        None
+                    }
+                    Ok((r, w)) => Some(Connection::new_from(r, w))
+                }
+            }
         }
     })
 }
