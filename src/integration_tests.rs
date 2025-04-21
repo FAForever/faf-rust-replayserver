3ec38707ddf8d8ab0b22f4959eaccbac3c6f4cdd
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2025-04-21 13:43:53 +0200

    Add integration test for websockets
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- src/integration_tests.rs
+++ src/integration_tests.rs
@@ -8,9 +8,12 @@ mod test {
 
     use std::{path::PathBuf, sync::Arc, time::Duration};
 
+    use futures::SinkExt;
+    use http::Uri;
     use rand::Rng;
     use tokio::{io::{AsyncReadExt, AsyncWriteExt}, join, net::TcpStream, select};
     use tokio_util::sync::CancellationToken;
+    use tokio_websockets::{ClientBuilder, Message};
 
     use crate::{config::test::default_config, server::server::server_with_real_deps, util::test::{compare_bufs, get_file, setup_logging, sleep_ms}};
 
@@ -73,6 +76,36 @@ mod test {
         output
     }
 
+    async fn websocket_writer(port: u16, header: &[u8], file: Vec<u8>) {
+        let uri = format!("ws://127.0.0.1:{}", port).parse::<Uri>().unwrap();
+        let (mut client, _) = ClientBuilder::from_uri(uri).connect().await.unwrap();
+
+        random_sleep().await;
+        client.send(Message::binary(header.to_owned())).await.unwrap();
+        random_sleep().await;
+        for data in file.chunks(1000) {
+                client.send(Message::binary(data.to_owned())).await.unwrap();
+                random_sleep().await;
+        }
+    }
+
+    async fn websocket_reader(port: u16, header: &[u8]) -> Vec<u8> {
+        let uri = format!("ws://127.0.0.1:{}", port).parse::<Uri>().unwrap();
+        let (mut client, _) = ClientBuilder::from_uri(uri).connect().await.unwrap();
+
+        // Wait for the writer to create a replay
+        tokio::time::sleep(Duration::from_millis(30)).await;
+        client.send(Message::binary(header.to_owned())).await.unwrap();
+        random_sleep().await;
+
+        let mut output: Vec<u8> = Default::default();
+        while let Some(Ok(msg)) = futures::StreamExt::next(&mut client).await {
+            assert!(msg.is_binary());
+            output.extend_from_slice(msg.as_payload());
+        }
+        output
+    }
+
     #[cfg_attr(not(feature = "local_db_tests"), ignore)]
     #[tokio::test(flavor = "multi_thread")]
     async fn integration_test_server_one_writer_one_reader() {
@@ -123,6 +156,62 @@ mod test {
         // This file was copied from this test and manually verified to be correct.
         // Zstd is deterministic for the same data + params + version, let's rely on that.
         let reference_replay = get_file("integration/game_2000.fafreplay");
+        compare_bufs(saved_replay, reference_replay);
+
+        // TODO: verify stats saved to the database.
+    }
+
+    #[cfg_attr(not(feature = "local_db_tests"), ignore)]
+    #[tokio::test(flavor = "multi_thread")]
+    async fn integration_test_server_websocket_one_writer_one_reader() {
+        // Game ID in db: 2000
+
+        setup_logging();
+
+        let replay_dir = tempfile::tempdir().unwrap();
+        let mut config = default_config();
+        config.server.port = None;
+        config.server.websocket_port = Some(0);
+        config.database.host = std::env::var("DB_HOST").unwrap();
+        config.database.port = str::parse::<u16>(&std::env::var("DB_PORT").unwrap()).unwrap();
+        config.storage.vault_path = replay_dir.path().to_str().unwrap().into();
+        config.server.connection_accept_timeout_s = Duration::from_millis(50);
+
+        let token = CancellationToken::new();
+        let (server, port_info) = server_with_real_deps(Arc::new(config), token.child_token()).await;
+
+        let sent_replay = get_file("example");
+        let replay_writer = websocket_writer(port_info.websocket.unwrap(), b"P/2001/foo\0", sent_replay.clone());
+        let replay_reader = websocket_reader(port_info.websocket.unwrap(), b"G/2001/foo\0");
+
+        let exit_server = async move || {
+            tokio::time::sleep(Duration::from_secs(1)).await;
+            token.cancel();
+        };
+
+        let run = async move || {
+            let (_, _, reader_data, _) = join! {
+               server.run(),
+               exit_server(),
+               replay_reader,
+               replay_writer,
+            };
+            reader_data
+        };
+        let reader_data = tokio::time::timeout(Duration::from_secs(2), run()).await.unwrap();
+        compare_bufs(reader_data, sent_replay);
+
+        let mut replay_path: PathBuf = replay_dir.path().to_owned();
+        replay_path.push("0");
+        replay_path.push("0");
+        replay_path.push("0");
+        replay_path.push("20");
+        replay_path.push("2001.fafreplay");
+        let saved_replay = std::fs::read(&replay_path).unwrap();
+
+        // This file was copied from this test and manually verified to be correct.
+        // Zstd is deterministic for the same data + params + version, let's rely on that.
+        let reference_replay = get_file("integration/game_2001.fafreplay");
         compare_bufs(saved_replay, reference_replay);
 
         // TODO: verify stats saved to the database.
